f7ebf04e9d4ebd6ef14295a3110fe00a69eb8544
vuser <vvvu@purdue.edu>
2020-12-18 20:44:07 -0500

    scalable image, comments

--- converter.py
+++ converter.py
@@ -8,20 +8,27 @@ try:
 
     image_file = args[0]
     output_file = args[1]
-    reducer = int(args[2])
+    reducer = np.int_(args[2])
 
     print ("Starting...")
 
+    # read and get size of image
     img = cv2.imread(image_file, 2)
-    write_file = open(output_file, "w")
-
     rows = len(img)
     cols = len(img[0])
+
+    # set up image scaling based on font size and line spacing
+    fontSize = 10
+    spacing = 1.1
+    scale = fontSize * 0.8 / reducer * spacing
     # create new image with white background
-    fontSize = 8
-    output_img = Image.new("L", (1920, 1080), color=0)
+    output_img = Image.new("L", (np.int_(cols * scale), np.int_(rows * scale)), color=0)
     draw = ImageDraw.Draw(output_img)
-    font = ImageFont.truetype("DejaVuSans.ttf", fontSize, encoding="unic")
+    # load ttf font
+    font = ImageFont.truetype("NotoMono-Regular.ttf", fontSize, encoding="unic")
+
+    # set up txt file to write to
+    write_file = open(output_file, "w")
 
     chars = " .*:+%S0#@"
     # defines the subsets of pixel intensities
@@ -29,21 +36,19 @@ try:
     div = np.amax(img) / (len(chars) - 1)
 
     for row in range(0, rows, reducer):
-        line = ""
+        currentRow = row * scale
         for col in range(0, cols, reducer):
             val = np.int_(img[row, col] / div)
-            line += chars[val]
-            draw.text((col * fontSize / reducer, row * fontSize / reducer), chars[val], 255, font=font)
-        # print(row)
-        # draw.text((0, row * fontSize), line, 255, font=font)
-        write_file.write(line)
+            write_file.write(chars[val] + " ")
+            # we must write to exact pixel location, as to avoid varying line lengths
+            draw.text((col * scale, currentRow), chars[val], 255, font=font)
         write_file.write("\n")
+    
+    # save the files
     write_file.close()
-
     output_img.save(output_file + ".png")
-
-    print ("Saved to " + output_file)
+    print ("Saved to " + output_file + ".png")
 except IndexError:
     print ("Invalid parameters. Make sure you have all parameters!")
-    print ("converter.py imageToConvert nameForOutput reducer")
+    print ("imageToConvert nameForOutput reducer")
 
