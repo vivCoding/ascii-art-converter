2d6efd0189f8bf3a51242dc9705ec6be6beff7d2
vuser <vivcoding@gmail.com>
2021-01-04 20:56:07 -0500

    heroku debuggin

--- app.py
+++ app.py
@@ -59,6 +59,7 @@ def convert():
         )
         p.start_process()
         jobs[file_id] = p
+        print (jobs)
         return jsonify(file_id), 200
     elif file_ext in VID_EXT:
         temp_batch_folder = os.path.join(TEMP, file_id + "/")
